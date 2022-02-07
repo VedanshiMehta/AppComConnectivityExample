95a9edaf1a8455db4748cb05d273155f6905ee0d
vedanshi.mehta <vedanshi.mehta@1R-76>
2022-02-07 18:01:41 +0530

    Add project files.

---
+++ AppComConnectivityExample/MainActivity.cs
@@ -0,0 +1,69 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace AppComConnectivityExample
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button myAppTheme;
+        Button myCompass;
+        Button myConnectivity;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+
+            myAppTheme = FindViewById<Button>(Resource.Id.button1);
+            myCompass = FindViewById<Button>(Resource.Id.button2);
+            myConnectivity = FindViewById<Button>(Resource.Id.button3);
+
+
+            myAppTheme.Click += MyAppTheme_Click;
+            myCompass.Click += MyCompass_Click;
+            myConnectivity.Click += MyConnectivity_Click;
+        }
+
+        private void MyConnectivity_Click(object sender, System.EventArgs e)
+        {
+            Intent k = new Intent(Application.Context, typeof(MyConnectivity));
+            StartActivity(k);
+        }
+
+        private void MyCompass_Click(object sender, System.EventArgs e)
+        {
+
+            Intent j = new Intent(Application.Context, typeof(MyCompass));
+            StartActivity(j);
+        }
+
+        private void MyAppTheme_Click(object sender, System.EventArgs e)
+        {
+            Intent i = new Intent(Application.Context, typeof(MyAppTheme));
+            StartActivity(i);
+        }
+
+        private void MyPropertyDemo_Click(object sender, System.EventArgs e)
+        {
+        
+        }
+
+      
+
+        
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ AppComConnectivityExample/MyAppTheme.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace AppComConnectivityExample
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    class MyAppTheme : Activity
+    {
+        private TextView myText;
+        private Button button;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.AppThemeDemo);
+            UIRefrence();
+            UIClickEvents();
+        }
+
+        private void UIClickEvents()
+        {
+            button.Click += Button_Click;
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            var currTheme = AppInfo.RequestedTheme;
+            myText.Text = $"CurrentTheme:{currTheme}";
+        }
+
+        private void UIRefrence()
+        {
+            button = FindViewById<Button>(Resource.Id.buttonAP);
+            myText = FindViewById<TextView>(Resource.Id.textViewAP);
+        }
+    }
+}
